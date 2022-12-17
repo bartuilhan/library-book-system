ee48bcf6ba37b10d28cc164cb9322c79648cbe81
Bartu Ilhan <bartuilhan834@gmail.com>
2022-12-17 17:37:31 +0300

    commit

--- library.py
+++ library.py
@@ -1,6 +1,14 @@
-
-name = input("what is name book:"),
-id = input("what call that book:"),
-year = input("what year of this book")
-
-
+num = 0
+num += 1
+name ="name:", input("what is name book:"),
+call ="call:" ,input("what call that book:"),
+year ="year:", input("what year of this book:")
+with open("data\\book.txt","w+") as file:
+    file.writelines("book")
+    file.writelines(name)
+    file.writelines("\n")
+    file.writelines(call)
+    file.writelines("\n")
+    file.writelines(year)
+    file.writelines("\n")
+    file.close()
--- test.py
+++
@@ -1,4 +0,0 @@
-name = input("what is name book:"),
-id = input("what call that book:"),
-year = input("what year of this book")
-
